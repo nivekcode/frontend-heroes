dc00f078e1ce1f7ea4e71027291df0334f23c7ed
kreuzerk <kevin.kreuzer90@icloud.com>
2020-09-09 17:15:05 +0200

    chore(main): refactor main logic

--- exercise-finished/main.ts
+++ exercise-finished/main.ts
@@ -8,36 +8,31 @@ const heroes: Hero[] = [
     {name: 'Spiderman', image: 'spiderman.jpeg'},
     {name: 'Superman', image: 'superman.jpeg'}
 ];
+
 const previousHeroBtn = document.getElementById('previous-hero-btn');
 const nextHeroBtn = document.getElementById('next-hero-btn');
 const heroImage = document.getElementById('hero-image') as HTMLImageElement;
-const heroTitle = document.getElementById('hero-title');
+const heroTitle = document.getElementById('hero-title') as HTMLTitleElement;
 
 let selectedHero = 0;
 
-const updateHero = (index: number) => {
+const selectPreviousHero = () => {
+    selectedHero = selectedHero === heroes.length - 1 ? 0 : selectedHero + 1;
+    updateHero(selectedHero, heroImage, heroTitle);
+}
+
+const selectNextHero = () => {
+    selectedHero = selectedHero === 0 ? heroes.length - 1 : selectedHero - 1;
+    updateHero(selectedHero, heroImage, heroTitle);
+}
+
+const updateHero = (heroIndex: number, heroImage: HTMLImageElement, heroTitle: HTMLTitleElement) => {
     if (heroTitle && heroImage) {
-
-        heroTitle.innerText = heroes[index].name;
-        heroImage.src = `./assets/${heroes[index].image}`;
-        heroImage.classList.add('animate__animated', 'animate__flipInX');
-
-        setTimeout(() => {
-            heroImage.classList.remove('animate__animated');
-            heroImage.classList.remove('animate__flipInX');
-        }, 500);
-
+        heroTitle.innerText = heroes[heroIndex].name;
+        heroImage.src = `./assets/${heroes[heroIndex].image}`;
     }
 };
 
-previousHeroBtn?.addEventListener('click', function () {
-    selectedHero = selectedHero === 0 ? heroes.length - 1 : selectedHero - 1;
-    updateHero(selectedHero);
-});
-
-nextHeroBtn?.addEventListener('click', function () {
-    selectedHero = selectedHero === heroes.length - 1 ? 0 : selectedHero + 1;
-    updateHero(selectedHero);
-});
-
-updateHero(0);
+previousHeroBtn?.addEventListener('click', selectNextHero);
+nextHeroBtn?.addEventListener('click', selectPreviousHero);
+updateHero(0, heroImage, heroTitle);
