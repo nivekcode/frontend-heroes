b74fb5af449c0ae46701e85a18db892d6170cac6
kreuzerk <kevin.kreuzer90@icloud.com>
2023-05-04 09:37:28 +0200

    feat: 🎸 update build exercise

---
+++ 5-exercise-npm-scripts-build/solution/main.ts
@@ -0,0 +1,36 @@
+interface Hero {
+    name: string;
+    image: string;
+}
+
+const heroes: Hero[] = [
+    {name: 'Batman', image: 'batman.svg'},
+    {name: 'Spiderman', image: 'spiderman.svg'},
+    {name: 'Superman', image: 'superman.svg'}
+];
+
+const previousHeroBtn = document.getElementById('previous-hero-btn');
+const nextHeroBtn = document.getElementById('next-hero-btn');
+const heroImage = document.getElementById('hero-image') as HTMLImageElement;
+const heroTitle = document.getElementById('hero-title') as HTMLTitleElement;
+
+let selectedHeroIndex = 0;
+
+const selectPreviousHero = () => {
+    selectedHeroIndex = selectedHeroIndex === 0 ? heroes.length - 1 : selectedHeroIndex - 1;
+    updateHero();
+}
+
+const selectNextHero = () => {
+    selectedHeroIndex = selectedHeroIndex === heroes.length - 1 ? 0 : selectedHeroIndex + 1;
+    updateHero();
+}
+
+const updateHero = () => {
+    heroTitle.innerText = heroes[selectedHeroIndex].name;
+    heroImage.src = `./assets/${heroes[selectedHeroIndex].image}`;
+};
+
+previousHeroBtn?.addEventListener('click', selectPreviousHero);
+nextHeroBtn?.addEventListener('click', selectNextHero);
+updateHero();
