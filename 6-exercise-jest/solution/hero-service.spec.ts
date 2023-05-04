b0f40dfd182c8ca74e757e3095bbc0d092764893
kreuzerk <kevin.kreuzer90@icloud.com>
2023-05-04 10:05:39 +0200

    refactor: 💡 remove exercise comment

--- 6-exercise-jest/solution/hero-service.spec.ts
+++ 6-exercise-jest/solution/hero-service.spec.ts
@@ -55,17 +55,4 @@ describe('HeroService', () => {
         });
 
     });
-
-    /*
-    TODO 5: Try to use test-driven development to
-    implement the following calculateHeroMissions method:
-
-     // HINT: To run a test you can either use the test npm script or the test:watch npm script.
-
-    The calculateHeroMissions method
-    accepts an array of heroes. It will then return an object that contains
-    the number of missions each hero has completed. So for example, if the
-    input is ['Batman', 'Spiderman', 'Batman', 'Superman'], it will return
-    {Batman: 2, Spiderman: 1, Superman: 1}.
-     */
 });
